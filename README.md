af86942261058d470ef20ecdc28605e95d8c7319
Mystical94 <shubhamgupta6428@gmail.com>
2022-08-17 19:33:17 +0530

    changes for deployment

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-## ``` Decentralized File Storage. ```
+## DropOpen: Your Decentralized File Storage!
+
+##DropOpen is a platform to store and share your data freely without any censorship and resistance.
+
+#Key Features:
+
+    Decentralized Storage on IPFS
+    Can Easily Share Files of different formats with Anyone in the World
+    Easy to Use for a Beginner
+
+#What problem are we solving:
 
--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 const privateKeys = process.env.PRIVATE_KEYS || ""
 
 module.exports = {
@@ -21,7 +23,15 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 3
-    }
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com/v1/4ea5c8fdabab3e528faa29c36085a5906c13591a`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      networkCheckTimeout: 100000,
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
